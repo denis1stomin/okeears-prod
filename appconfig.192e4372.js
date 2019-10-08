a13c5ab4a77875d19e9f298fab8ef5bca110207a
Denis <igehuc.dev@gmail.com>
2019-10-08 23:07:42 +0300

    Manually deployed the last master commit

--- appconfig.192e4372.js
+++ appconfig.192e4372.js
@@ -68,6 +68,6 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({2:[function(require,module,exports) {
+})({7:[function(require,module,exports) {
 window.AppConfig={auth:{clientId:"6f9caa6b-b5cf-467d-a326-f25cb0aca8f2"},app:{autoShareNotebook:!1,checkNotebookIsShared:!0},instrumentationKey:"0a12fbd1-0c70-41d3-87d8-34cbd47955fb"};
-},{}]},{},[2])
+},{}]},{},[7])
